e7be6f02be010a1b695352ad8725435c2df928a8
Bhuvan Gabbita <59176326+notbhuvangab@users.noreply.github.com>
2025-05-06 01:49:35 -0400

    Removed Eslist for builts

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   "resolveJsonModule": true,
+  ignoreDuringBuilds: true
 };
 
 export default nextConfig;
